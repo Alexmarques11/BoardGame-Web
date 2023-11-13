ce58aedd6a1531edd533b4855a90ff9887c7013a
Alexandre Marques <a25968@alunos.ipca.pt>
2023-11-13 11:39:22 +0000

    Atualizado

--- brain.js
+++ brain.js
@@ -16,10 +16,19 @@ let posicaoY = 0;
 let emMovimento = false; // Variável para controlar se a personagem está em movimento
 
 //Tempo Inicial do jogo
-let tempoRestante = 10;
+let tempoRestante = 100;
+
+let score = 0;
+let vidas = 10; // Ou qualquer valor inicial que você desejar
 
 let jogoIniciado = false;
 let pausado = false; // Variável para controlar o estado de pausa do jogo
+
+let inventario = [
+  { tecla: 1, acao: "Ação 1" },
+  { tecla: 2, acao: "Ação 2" },
+  { tecla: 3, acao: "Ação 3" }
+];
 
 // Função para mover a personagem
 function moverPersonagem(direcao) {
@@ -69,14 +78,14 @@ function moverPersonagem(direcao) {
 
   console.log(`Nova posição: (${posicaoX}, ${posicaoY})`);
 
+  adicionarPontos(1);
   // Lidar com o término da animação
   setTimeout(() => {
     emMovimento = false; // Marcar que o movimento terminou
+    gerarQuadrados();
+    verificarColisao(); // Verificar colisão após o movimento
   }, velocidadeMovimento);
-
-  gerarQuadrados();
 }
-
 
 
 // Lidar com eventos de pressionamento de tecla para as setas do teclado
@@ -105,10 +114,121 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
+function verificarColisao() {
+  const personagemBounds = personagem.getBoundingClientRect();
+  const quadrados = document.querySelectorAll('.square');
+  const estrelas = document.querySelectorAll('.four-pointed-star');
+
+  quadrados.forEach(quadrado => {
+    const quadradoBounds = quadrado.getBoundingClientRect();
+
+    if (
+      personagemBounds.left < quadradoBounds.right &&
+      personagemBounds.right > quadradoBounds.left &&
+      personagemBounds.top < quadradoBounds.bottom &&
+      personagemBounds.bottom > quadradoBounds.top
+    ) {
+      const corQuadrado = quadrado.style.backgroundColor;
+
+      switch (corQuadrado) {
+        case 'rgb(255, 255, 255)':
+          // Branco: Adiciona 1 ponto ao score
+          adicionarPontos(1);
+          break;
+        case 'rgb(255, 255, 0)':
+          // Amarelo: Move o quadrado para dentro do inventário
+          moverParaInventario(quadrado);
+          break;
+        case 'rgb(0, 128, 0)':
+          // Verde: Adiciona 5 pontos ao score
+          adicionarPontos(5);
+          break;
+        case 'rgb(0, 0, 255)':
+          // Azul: Tira 1 vida
+          removerVida(1);
+          break;
+        case 'rgb(255, 0, 0)':
+          // Vermelho: Tira 2 vidas
+          removerVida(2);
+          break;
+      }
+
+      quadrado.remove();
+    }
+  });
+
+  estrelas.forEach(estrela => {
+    const estrelaBounds = estrela.getBoundingClientRect();
+
+    if (
+      personagemBounds.left < estrelaBounds.right &&
+      personagemBounds.right > estrelaBounds.left &&
+      personagemBounds.top < estrelaBounds.bottom &&
+      personagemBounds.bottom > estrelaBounds.top
+    ) {
+      // Colisão com a estrela: Mover a estrela para o inventário
+      moverParaInventario(estrela);
+      // Adicionar lógica específica da estrela, se necessário
+      // Por exemplo, adicionar pontos extras ou realizar outra ação
+      estrela.remove();
+    }
+  });
+}
+
+
+//cria a função para adicionar pontos mas não uses a que estava ai
+function adicionarPontos(quantidade) {
+  score += quantidade;
+
+  // Atualizar a exibição de pontos na interface
+  const pontuacaoElement = document.getElementById('pontuacao'); // Substitua 'pontuacao' pelo ID correto
+  if (pontuacaoElement) {
+    pontuacaoElement.textContent = `${score}`;
+  }
+
+  console.log(`Adicionando ${quantidade} pontos. Nova pontuação: ${score}`);
+}
+
+
+// Função para remover vidas
+function removerVida(quantidade) {
+  vidas -= quantidade;
+
+  // Atualizar a exibição de vidas na interface
+  const vidasElement = document.getElementById('vidas'); // Substitua 'vidas' pelo ID correto
+  if (vidasElement) {
+    vidasElement.textContent = `${vidas}`;
+  }
+
+  // Verificar se o jogo acabou (quando não há mais vidas)
+  if (vidas <= 0) {
+    atualizarTemporizador(vidas);
+  }
+
+  console.log(`Removendo ${quantidade} vidas. Novo total de vidas: ${vidas}`);
+}
+
+function gerarEstrela() {
+  const celulas = document.querySelectorAll('.celula');
+  const indiceCelula = Math.floor(Math.random() * celulas.length);
+  const celula = celulas[indiceCelula];
+
+  const estrela = document.createElement('div');
+  estrela.classList.add('four-pointed-star');
+
+  celula.appendChild(estrela);
+}
+
 function gerarQuadrados(posX, posY) {
-  // Remova todos os quadrados existentes
-  const quadradosExistents = document.querySelectorAll('.square');
-  quadradosExistents.forEach(quadrado => quadrado.remove());
+  // Remova todos os quadrados e estrelas existentes, exceto os do inventário
+  const quadradosEstrelasExistents = document.querySelectorAll('.square, .four-pointed-star');
+  quadradosEstrelasExistents.forEach(item => {
+    const celulaPai = item.closest('.celula');
+    if (celulaPai && celulaPai.classList.contains('inventario')) {
+      return; // Ignora itens no inventário
+    }
+    item.remove();
+  });
 
   // Gere três quadrados aleatórios em locais aleatórios do tabuleiro
   const celulas = document.querySelectorAll('.celula');
@@ -119,20 +239,28 @@ function gerarQuadrados(posX, posY) {
     const celulaX = parseInt(celula.dataset.x);
     const celulaY = parseInt(celula.dataset.y);
 
-    // Verifique se a célula é a posição da personagem ou já contém um quadrado
-    if ((celulaX === posX && celulaY === posY) || celula.querySelector('.square')) {
-      i--; // Se a célula escolhida já contém um quadrado ou é a posição da personagem, gere outra
+    // Verifique se a célula é a posição da personagem ou já contém um quadrado ou estrela
+    if ((celulaX === posX && celulaY === posY) || celula.querySelector('.square') || celula.querySelector('.four-pointed-star') || celula.classList.contains('inventario')) {
+      i--; // Se a célula escolhida já contém um quadrado, estrela, está no inventário ou é a posição da personagem, gere outra
       continue;
     }
 
-    const quadrado = document.createElement('div');
-    quadrado.classList.add('square');
+    if (score > 0 && score % 7 === 0 && i === 0) {
+      // Se for um múltiplo de 7, gere a estrela
+      const estrela = document.createElement('div');
+      estrela.classList.add('four-pointed-star');
+      celula.appendChild(estrela);
+    } else {
+      // Caso contrário, gere um quadrado
+      const quadrado = document.createElement('div');
+      quadrado.classList.add('square');
 
-    // Adicione uma função para gerar uma cor aleatória
-    const corAleatoria = gerarCorAleatoria();
-    quadrado.style.backgroundColor = corAleatoria;
+      // Adicione uma função para gerar uma cor aleatória
+      const corAleatoria = gerarCorAleatoria();
+      quadrado.style.backgroundColor = corAleatoria;
 
-    celula.appendChild(quadrado);
+      celula.appendChild(quadrado);
+    }
   }
 }
 
@@ -147,12 +275,12 @@ function gerarCorAleatoria() {
 }
 
 
-function atualizarTemporizador() {
+function atualizarTemporizador(vidas) {
   tempoRestante--; // Reduz o tempo restante em 1 segundo
   const tempoElement = document.getElementById('tempo');
   const gameOverElement = document.getElementById('game-over');
 
-  if (tempoRestante <= 0) {
+  if (tempoRestante <= 0 || vidas <= 0) {
     clearInterval(temporizador); // Para o temporizador quando o tempo acaba
     tempoElement.textContent = 'Game Over!'; // Exibe "Game Over"
     gameOverElement.style.display = 'block'; // Exibe o elemento "Game Over"
@@ -178,7 +306,7 @@ function iniciarTemporizador() {
 // Função para iniciar o jogo
 function iniciarJogo() {
   // Redefina as variáveis do jogo
-  tempoRestante = 10; // Defina o tempo inicial novamente (ou o valor que desejar)
+  tempoRestante = 100; // Defina o tempo inicial novamente (ou o valor que desejar)
   posicaoX = 0; // Posição X inicial da personagem
   posicaoY = 0; // Posição Y inicial da personagem
   const startButton = document.getElementById('start');
@@ -187,6 +315,14 @@ function iniciarJogo() {
   // Oculte o "Game Over"
   const gameOverElement = document.getElementById('game-over');
   gameOverElement.style.display = 'none';
+
+  vidas = 10; 
+  // Atualize a exibição de vidas na interface
+  const vidasElement = document.getElementById('vidas'); // Substitua 'vidas' pelo ID correto
+  if (vidasElement) {
+    vidasElement.textContent = `${vidas}`; 
+  }
+  score=0;
 
   // Reposicione a personagem para a posição inicial
   personagem.style.transform = 'translate(0, 0)'; // Mova de volta para a posição inicial
@@ -379,9 +515,48 @@ const botaoSkin = document.getElementById("skin");
 botaoSkin.addEventListener("click", mudarSkin);
 
 
+document.addEventListener("keydown", (event) => {
+  // ... (seu código existente)
 
+  switch (event.key) {
+    case "1":
+      executarAcaoInventario(1);
+      break;
+    case "2":
+      executarAcaoInventario(2);
+      break;
+    case "3":
+      executarAcaoInventario(3);
+      break;
+    // ... (seu código existente)
+  }
+});
 
+function executarAcaoInventario(tecla) {
+  const itemInventario = inventario.find(item => item.tecla === tecla);
 
+  if (itemInventario) {
+    console.log(`Executando ação do inventário: ${itemInventario.acao}`);
+    // Adicione aqui a lógica específica para cada ação do inventário
+  }
+}
 
+function moverParaInventario(item) {
+  const inventarioCelula = document.querySelector('.inventario td:first-child');
 
+  if (inventarioCelula) {
+    const corItem = item.style.backgroundColor;
+    const novoItem = document.createElement('div');
+    
+    // Verifica se é um quadrado ou uma estrela e adiciona a classe apropriada
+    if (item.classList.contains('square')) {
+      novoItem.classList.add('square');
+    } else if (item.classList.contains('four-pointed-star')) {
+      novoItem.classList.add('four-pointed-star');
+    }
 
+    novoItem.style.backgroundColor = corItem;
+
+    inventarioCelula.appendChild(novoItem);
+  }
+}
